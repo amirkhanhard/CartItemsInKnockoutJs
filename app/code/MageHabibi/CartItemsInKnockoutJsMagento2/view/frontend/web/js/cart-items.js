22b9c8baad424ccd3721fa4ca403b302e254d28c
amirkhanhard <65039152+amirkhanhard@users.noreply.github.com>
2021-01-19 01:26:37 +0530

    Create cart-items.js

---
+++ app/code/MageHabibi/CartItemsInKnockoutJsMagento2/view/frontend/web/js/cart-items.js
@@ -0,0 +1,69 @@
+define([
+
+    'jquery',
+
+    'ko',
+
+    'uiComponent',
+
+    'Magento_Customer/js/customer-data',
+
+    'uiRegistry'
+
+], function ($, ko, Component, customerData, registry) {
+
+    'use strict';
+
+    return Component.extend({
+
+        defaults: {
+
+            template: 'MageHabibi_CartItemsInKnockoutJsMagento2/product/cart-items'
+
+        },
+
+        observableProperties: [
+
+            'items'
+
+        ],
+
+        /** @inheritdoc */
+
+        initialize: function () {
+
+            this._super();
+
+            var self = this;
+
+            customerData.get('cart').subscribe(
+
+                function (cartData) {
+
+                    console.log(cartData);
+
+                    self.items(cartData.items);
+
+                }
+
+            );
+
+            this.items(customerData.get('cart')().items); //get cart items
+
+            console.log(this.items());
+
+        },
+
+        initObservable: function () {
+
+            this._super();
+
+            this.observe(this.observableProperties);
+
+            return this;
+
+        }
+
+    });
+
+});
